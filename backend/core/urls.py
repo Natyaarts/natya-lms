9841c015bb9048ddb910febd3d159c650c2f62c3
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-07-30 17:59:09 +0530

    fix: add health check endpoint for EB

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path('', health_check, name='health_check'),
     path('admin/', admin.site.urls),
     path('api/courses/', include('courses.urls')),
     path('api/orders/', include('orders.urls')),
