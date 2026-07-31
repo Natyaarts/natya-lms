f4a0a10923a1b2e7d2883a704384861dc33c0b3c
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-07-31 10:47:08 +0530

    fix: increase JWT token lifetime to 30 days to prevent auto logout

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,6 +139,12 @@ TEMPLATES = [
     },
 ]
 
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+}
+
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
